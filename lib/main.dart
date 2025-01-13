3e6a64afa889c89c8a93348b925dd40945736c37
Tristoung <tristoung@gmail.com>
2025-01-13 12:31:42 +0100

    recuperation de donnees par l'api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -76,23 +78,73 @@ class CharacterTile extends StatelessWidget {
   }
 }
 
+const apiLink = 'https://www.swapi.tech/api/';
+
 class BrainrotService {
+
+
   Future<List<String>> fetchBrainrot() async {
     await Future.delayed(
       Duration(seconds: 3),
     );
-    return [
-      'Snailminem',
-      'Red apple dog',
-      'Quandale Dingle',
-      'The eye of Rah',
-    ];
+
+    final response = await http.get(Uri.parse('${apiLink}people'));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      if (json is Map<String, dynamic>) {
+        return _parseCharacters(json).map((e) => e.name).toList();
+      }
+    }
+    return[];
   }
+  List<BasicCharacterData> _parseCharacters(Map<String, dynamic> json) {
+    final results = json['results'];
+    if (results is List<dynamic>) {
+      return results
+      .map(
+        (json) => BasicCharacterData.fromJson(json as Map<String, dynamic>),
+        )
+        .toList();
+    } else {
+      return [];
+    }
+  }
+  // Future<String> fetchMoreBrainrot() async {
+  //   await Future.delayed(
+  //     Duration(seconds: 1),
+  //   );
+  //   if(_names.isNotEmpty) {
+  //     String nameToSend = _names.removeLast();
+  //     return nameToSend;
+  //   }
+  //   return "";
+  // }
 }
 
+class BasicCharacterData {
+  final String id;
+  final String name;
+
+  BasicCharacterData({
+    required this.id,
+    required this.name,
+  });
+
+  BasicCharacterData.fromJson(Map<String, dynamic> json)
+    : id = json['uid'],
+      name = json['name'];
+}
+
+// return [
+//   'Snailminem',
+//   'Red apple dog',
+//   'Quandale Dingle',
+//   'The eye of Rah',
+// ];
+
 // final _names = [
-//     'Property in Egypt',
-//     'John Pork',
-//     'Chopped Chin',
-//     'The Rizzler'
-//   ];
+//   'Property in Egypt',
+//   'John Pork',
+//   'Chopped Chin',
+//   'The Rizzler'
+// ];
