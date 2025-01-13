5d15c97377a73165ad3bc3ff19a9e7f4fcc066af
Tristoung <tristoung@gmail.com>
2025-01-13 11:59:12 +0100

    on fetch la liste en asynchrone

--- lib/main.dart
+++ lib/main.dart
@@ -32,18 +32,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final _names = [
-    'Property in Egypt',
-    'John Pork',
-    'Chopped Chin',
-  ];
+  final _service = BrainrotService();
+  List<String> _names = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _service.fetchBrainrot().then((names) {
+      setState(() {
+        _names = names;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         title: const Text('Brainrot List'),
       ),
       body: ListView.builder(
@@ -63,7 +70,29 @@ class CharacterTile extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return ListTile(
+      titleTextStyle: TextStyle(fontSize: 15, color: Colors.black),
       title: Text(name),
     );
   }
 }
+
+class BrainrotService {
+  Future<List<String>> fetchBrainrot() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+    return [
+      'Snailminem',
+      'Red apple dog',
+      'Quandale Dingle',
+      'The eye of Rah',
+    ];
+  }
+}
+
+// final _names = [
+//     'Property in Egypt',
+//     'John Pork',
+//     'Chopped Chin',
+//     'The Rizzler'
+//   ];
